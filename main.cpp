840905f1d5de905a94c7e4921fab93073a3f6c81
Anda Stefania Ghita <104030121+AndaGhita@users.noreply.github.com>
2023-02-10 10:12:10 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+
+#include "deck.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    Deck deck = Deck();
+    cout << "Fresh deck: " << deck << endl;
+    deck.shuffle();
+    cout << "Shuffled deck: " << deck << endl;
+    deck.sort();
+    cout << "Sorted deck: " << deck << endl;
+   
+  return 0;
+}
